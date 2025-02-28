59ecd5cb78641a787dd56db2518670e03ee6d311
DorsaBeigifard <dorsabeigi@gmail.com>
2025-02-28 18:29:18 +0330

    Fetched Data

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,48 @@ import CharacterList from "../components/CharacterList";
 import NavBar, { SearchResult } from "../components/NavBar";
 import "./App.css";
 import { allCharacters } from "../data/data";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
+
 export default function App() {
-  const [characters, setCharecters] = useState(allCharacters);
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      console.log("Fetching data...");
+      setIsLoading(true);
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.json();
+      setCharacters(data.results.slice(0, 5));
+      setIsLoading(false);
+      console.log("Data received:", data);
+    }
+
+    fetchData();
+  }, []);
+
+  // Second Way
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://rickandmortyapi.com/api/character")
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       setCharacters(data.results.slice(0, 5));
+  //       setIsLoading(false);
+  //     }));
+  // }, []);
 
   return (
     <div className="app">
       {/* Component Composition for dealing with props drilling */}
       <NavBar>
         <SearchResult numOfResult={characters.length} />
-        
       </NavBar>
       <Main>
-        <CharacterList characters={characters} />
+        {isLoading ? <Loader /> : <CharacterList characters={characters} />}
         <CharacterDetail />
       </Main>
     </div>
