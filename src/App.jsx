a6de204a7cce642395018fe16465e3ccee78af2a
DorsaBeigifard <dorsabeigi@gmail.com>
2025-02-28 19:37:06 +0330

    used axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import CharacterDetail from "../components/CharacterDetail";
 import CharacterList from "../components/CharacterList";
 import NavBar, { SearchResult } from "../components/NavBar";
 import "./App.css";
-import { allCharacters } from "../data/data";
 import { useEffect, useState } from "react";
 import Loader from "../components/Loader";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 
 export default function App() {
   const [characters, setCharacters] = useState([]);
@@ -12,33 +13,29 @@ export default function App() {
 
   useEffect(() => {
     async function fetchData() {
-      console.log("Fetching data...");
-      setIsLoading(true);
-      const res = await fetch("https://rickandmortyapi.com/api/character");
-      const data = await res.json();
-      setCharacters(data.results.slice(0, 5));
-      setIsLoading(false);
-      console.log("Data received:", data);
+      // Using trycatch for error handling
+      try {
+        console.log("Fetching data...");
+        setIsLoading(true);
+        const { data } = await axios.get(
+          "https://rickandmortyapi.com/api/character"
+        );
+        setCharacters(data.results.slice(0, 5));
+        console.log("Data received:", data);
+      } catch (error) {
+        console.log(error.response.data.error);
+        toast.error(error.response.data.error);
+      } finally {
+        setIsLoading(false);
+      }
     }
 
     fetchData();
   }, []);
 
-  // Second Way
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   fetch("https://rickandmortyapi.com/api/character")
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((data) => {
-  //       setCharacters(data.results.slice(0, 5));
-  //       setIsLoading(false);
-  //     }));
-  // }, []);
-
   return (
     <div className="app">
+      <Toaster />
       {/* Component Composition for dealing with props drilling */}
       <NavBar>
         <SearchResult numOfResult={characters.length} />
